c95ae2473ae6e568b8f9449588c819a02cf5c325
Pedro teixeira <pedroteixeirabisognin@gmail.com>
2020-05-11 09:29:04 -0300

    Aula 4 concluida

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,95 @@
+<!-- alurapic/src/App.vue -->
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos" v-bind:key="foto">
-        <img :src="foto.url" :alt="foto.titulo" />
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos" v-bind:key="foto">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
+
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    "meu-painel": Painel
+  },
   data() {
     return {
       titulo: "Alurapic",
-      fotos: [
-        {
-          url:
-            "https://s2.glbimg.com/XGiwiDG6MIsdX_1CwCXbwn385Y4=/0x0:800x450/1600x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg",
-          titulo: "cachorro"
-        },
-        {
-          url:
-            "https://s2.glbimg.com/XGiwiDG6MIsdX_1CwCXbwn385Y4=/0x0:800x450/1600x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg",
-          titulo: "cachorrao"
-        }
-      ]
+      fotos: []
     };
-  },created() {
-
+  },
+  created() {
     //$http só funciona pois foi instânciado em main js VueResource
-     this.$http.get('http://localhost:3000/v1/fotos')
-     //Ao receber a resposta da api converta o resultado para json
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      //Ao receber a resposta da api converta o resultado para json
       .then(res => res.json())
-      //Após retornar o resultado em json insira o retorno dentro 
+      //Após retornar o resultado em json insira o retorno dentro
       //de fotos criado em data()
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-
+      .then(
+        fotos => (this.fotos = fotos),
+        err => console.log(err)
+      );
   }
 };
 </script>
 
 <style>
+.titulo {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.painel * {
+  box-shadow: 5px 5px 5px;
+}
 </style>
